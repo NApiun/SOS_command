dbd80bc01a9977355bf45eee289a4c4cc2187bee
NApiun <152114279+NApiun@users.noreply.github.com>
2024-09-15 18:49:03 -0300

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,46 @@
+void setup(){
+  pinMode (8, OUTPUT);
+}
+void loop(){
+  digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+
+  digitalWrite (8, HIGH);
+  delay (3000);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (3000);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (3000);
+  digitalWrite (8, LOW);
+  delay (500);
+
+    digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+  digitalWrite (8, HIGH);
+  delay (500);
+  digitalWrite (8, LOW);
+  delay (500);
+
+  delay (5000);
+
+}
